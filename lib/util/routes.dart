b00bc5ca0d93798ab21b380b048b8863e3956a82
Daria <jonshilin.gg@gmail.com>
2025-04-22 20:09:00 +0300

    feat: add routes

---
+++ lib/util/routes.dart
@@ -0,0 +1,11 @@
+class Routes {
+  static const onboarding1 = '/onboarding1';
+  static const onboarding2 = '/onboarding2';
+  static const onboarding3 = '/onboarding3';
+  static const onboarding4 = '/onboarding4';
+  static const auth = '/auth';
+  static const root = '/';
+  static const verify = '/verify';
+  static const role = '/role';
+  static const projects = '/projects';
+}

3c8ecf7161ab29be5ce0a867ee1c2f7e60cad608
Daria <jonshilin.gg@gmail.com>
2025-04-22 20:14:44 +0300

    feat: add new class user_dto

---
+++ lib/model/user_dto.dart
@@ -0,0 +1,22 @@
+class UserDto {
+  final int id;
+  final String firstName;
+  final String lastName;
+  final String email;
+
+  UserDto({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+  });
+
+  factory UserDto.fromJson(Map<String, dynamic> json) {
+    return UserDto(
+      id: json['id'],
+      firstName: json['name'],
+      lastName: json['surname'],
+      email: json['email'],
+    );
+  }
+}

d6a9719b20a424487182ae8fd2929f6e096e53b3
Daria <jonshilin.gg@gmail.com>
2025-04-22 20:14:59 +0300

    feat: add new class auth_response.dart

---
+++ lib/model/auth_response.dart
@@ -0,0 +1,15 @@
+import 'user_dto.dart';
+
+class AuthResponse {
+  final String token;
+  final UserDto user;
+
+  AuthResponse({required this.token, required this.user});
+
+  factory AuthResponse.fromJson(Map<String, dynamic> json) {
+    return AuthResponse(
+      token: json['token'],
+      user: UserDto.fromJson(json['user']),
+    );
+  }
+}

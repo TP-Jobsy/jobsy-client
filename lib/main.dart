84506caec84f99e5c5e03c19049afd1ffa769df7
exactly228 <91571554+exactly228@users.noreply.github.com>
2025-05-04 01:33:25 +0300

    refactor: delete doesnt exist class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';

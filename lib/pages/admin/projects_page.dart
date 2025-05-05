4ae140a9d8d66a9fad4eddc03e0857ac8def8700
exactly228 <91571554+exactly228@users.noreply.github.com>
2025-05-05 05:28:49 +0300

    refactor: change navigation

--- lib/pages/admin/projects_page.dart
+++ lib/pages/admin/projects_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:jobsy/pages/admin/sidebar.dart';
 import '../../service/mock_admin_service.dart';
 import 'abstract_table_page.dart';
+import 'project_detail_page.dart';
 
 class ProjectsPage extends StatelessWidget {
   const ProjectsPage({Key? key}) : super(key: key);
@@ -23,9 +24,10 @@ class ProjectsPage extends StatelessWidget {
       buildRow: (p) {
         final d = p.createdAt;
         final date =
-            '${d.day.toString().padLeft(2,'0')}.'
-            '${d.month.toString().padLeft(2,'0')}.'
+            '${d.day.toString().padLeft(2, '0')}.'
+            '${d.month.toString().padLeft(2, '0')}.'
             '${d.year}';
+
         return DataRow(cells: [
           DataCell(Text(p.id.toString())),
           DataCell(Text(p.title)),
@@ -35,7 +37,14 @@ class ProjectsPage extends StatelessWidget {
           DataCell(Text(date)),
           DataCell(IconButton(
             icon: const Icon(Icons.arrow_forward_ios, size: 16),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => ProjectDetailPage(project: p),
+                ),
+              );
+            },
           )),
         ]);
       },

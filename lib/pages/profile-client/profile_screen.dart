26089e86a371eecc1a562889a6521cb7e013ce42
Arpine Shkhikian <arpine985985@gmail.com>
2025-05-13 16:33:20 +0300

    feat: fix profile screen

--- lib/pages/profile-client/profile_screen.dart
+++ lib/pages/profile-client/profile_screen.dart
@@ -50,17 +50,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
         backgroundColor: Palette.white,
         foregroundColor: Palette.black,
         elevation: 0,
-        leading: IconButton(
-          icon: SvgPicture.asset(
-            'assets/icons/ArrowLeft.svg',
-            width: 20,
-            height: 20,
-            color: Palette.navbar,
-          ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
+        leading: null,
+          automaticallyImplyLeading: false
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),

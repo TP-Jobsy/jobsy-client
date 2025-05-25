f5e06498dd47dd3e958d14f0b57a3951aa184a90
sinemoraa <jonshilin.gg@gmail.com>
2025-05-25 04:03:08 +0300

    feat: refactor AvatarService to use token handling for avatar uploads

--- lib/service/avatar_service.dart
+++ lib/service/avatar_service.dart
@@ -3,62 +3,74 @@ import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
 import 'package:mime/mime.dart';
 import 'package:path/path.dart' as p;
-
 import '../util/routes.dart';
+
+typedef TokenGetter    = Future<String?> Function();
+typedef TokenRefresher = Future<void> Function();
 
 class AvatarService {
   final String _baseUrl;
   final http.Client _http;
+  final TokenGetter _getToken;
+  final TokenRefresher _refreshToken;
 
-  AvatarService({String? baseUrl, http.Client? httpClient})
-      : _baseUrl = baseUrl ?? Routes.apiBase,
-        _http = httpClient ?? http.Client();
+  AvatarService({
+    String? baseUrl,
+    http.Client? httpClient,
+    required TokenGetter getToken,
+    required TokenRefresher refreshToken,
+  })  : _baseUrl       = baseUrl ?? Routes.apiBase,
+        _http          = httpClient ?? http.Client(),
+        _getToken      = getToken,
+        _refreshToken  = refreshToken;
 
-  Future<String> uploadClientAvatar({
-    required String token,
-    required File file,
-  }) async {
-    final uri = Uri.parse('$_baseUrl/profile/client/avatar');
-    return _upload(token: token, file: file, uri: uri);
-  }
+  Future<String> uploadClientAvatar({ required File file }) =>
+      _upload(path: '/profile/client/avatar', file: file);
 
-  Future<String> uploadFreelancerAvatar({
-    required String token,
-    required File file,
-  }) async {
-    final uri = Uri.parse('$_baseUrl/profile/freelancer/avatar');
-    return _upload(token: token, file: file, uri: uri);
-  }
+  Future<String> uploadFreelancerAvatar({ required File file }) =>
+      _upload(path: '/profile/freelancer/avatar', file: file);
 
   Future<String> _upload({
-    required String token,
+    required String path,
     required File file,
-    required Uri uri,
   }) async {
+    final uri = Uri.parse('$_baseUrl$path');
     final mimeType = lookupMimeType(file.path);
     if (mimeType == null || !mimeType.startsWith('image/')) {
       throw Exception('Неподдерживаемый формат файла');
     }
-    final parts = mimeType.split('/'); // ['image','png']
-    final filename = p.basename(file.path);
+    final parts   = mimeType.split('/');
+    final filename= p.basename(file.path);
 
-    final req = http.MultipartRequest('POST', uri)
-      ..headers['Authorization'] = 'Bearer $token'
-      ..files.add(
-        http.MultipartFile(
-          'file',
-          file.openRead(),
-          await file.length(),
-          filename: filename,
-          contentType: MediaType(parts[0], parts[1]),
-        ),
-      );
-
-    final streamed = await req.send();
-    final res = await http.Response.fromStream(streamed);
-    if (res.statusCode != 200) {
-      throw Exception('Ошибка при загрузке аватара: ${res.body}');
+    Future<http.Response> send(String? token) async {
+      final req = http.MultipartRequest('POST', uri)
+        ..headers['Authorization'] = token != null ? 'Bearer $token' : ''
+        ..files.add(
+          http.MultipartFile(
+            'file',
+            file.openRead(),
+            await file.length(),
+            filename: filename,
+            contentType: MediaType(parts[0], parts[1]),
+          ),
+        );
+      final streamed = await req.send();
+      return http.Response.fromStream(streamed);
     }
+
+    String? token = await _getToken();
+    http.Response res = await send(token);
+
+    if (res.statusCode == 401) {
+      await _refreshToken();
+      token = await _getToken();
+      res   = await send(token);
+    }
+
+    if (res.statusCode != 200) {
+      throw Exception('Ошибка при загрузке аватара: ${res.statusCode} ${res.body}');
+    }
+
     return res.body;
   }
 }

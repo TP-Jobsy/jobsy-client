0ffadbc62e38695782cca56310670d9af2e6a373
sinemoraa <jonshilin.gg@gmail.com>
2025-05-25 04:02:49 +0300

    feat: refactor AuthResponse to support access and refresh tokens with expiry handling

--- lib/model/auth/auth_response.dart
+++ lib/model/auth/auth_response.dart
@@ -1,24 +1,31 @@
 import '../user.dart';
 
 class AuthResponse {
-  final String token;
+  final String accessToken;
+  final String refreshToken;
+  final DateTime refreshTokenExpiry;
   final UserDto user;
 
-  AuthResponse({required this.token, required this.user});
+  AuthResponse({
+    required this.accessToken,
+    required this.refreshToken,
+    required this.refreshTokenExpiry,
+    required this.user,
+  });
 
   factory AuthResponse.fromJson(Map<String, dynamic> json) {
-    final userJson = (json['user'] as Map<String, dynamic>?) ?? {};
     return AuthResponse(
-      token: json['token'],
-      user: UserDto(
-        id: userJson['id'].toString(),
-        email: userJson['email'] ?? '',
-        firstName: userJson['firstName'] ?? '',
-        lastName: userJson['lastName'] ?? '',
-        role: userJson['role'] ?? '',
-        phone: userJson['phone'] ?? '',
-        dateBirth: userJson['dateBirth'] ?? '',
-      ),
+      accessToken: json['accessToken'] as String,
+      refreshToken: json['refreshToken'] as String,
+      refreshTokenExpiry: DateTime.parse(json['refreshTokenExpiry'] as String),
+      user: UserDto.fromJson(json['user'] as Map<String,dynamic>),
     );
   }
+
+  Map<String, dynamic> toJson() => {
+    'accessToken': accessToken,
+    'refreshToken': refreshToken,
+    'refreshTokenExpiry': refreshTokenExpiry.toUtc().toIso8601String(),
+    'user': user.toJson(),
+  };
 }
